b11e24e2b55ba51f545f7aa025f46ce2f9647b41
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2024-05-06 14:59:53 +0300

    Update Input

--- Assets/Core/Scripts/PlayerInput.cs
+++ Assets/Core/Scripts/PlayerInput.cs
@@ -1,3 +1,5 @@
+using PlanetMerge.Planet;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +9,40 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private Transform _launchPoint;
     [SerializeField] private Rigidbody2D _planetPrefab;
     [SerializeField] private float _force;
+    [SerializeField] private float _launchCooldownl;
+
+    private Rigidbody2D _currentPlanet;
+    private Coroutine _launchRoutine;
+
+    private void Awake()
+    {
+        CreatePlanet();
+    }
+
+    private void CreatePlanet()
+    {
+        _currentPlanet = Instantiate(_planetPrefab, _launchPoint.position, Quaternion.identity);
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 direction = (mousePosition - _launchPoint.position).normalized;
-
-            var planet = Instantiate(_planetPrefab, _launchPoint.position, Quaternion.identity);
-            planet.AddForce(direction * _force, ForceMode2D.Impulse);
+            if (_launchRoutine != null)
+                _launchRoutine = StartCoroutine(LaunchPlanet());
         }
+    }
+
+    private IEnumerator LaunchPlanet()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = (mousePosition - _launchPoint.position).normalized;
+
+        _currentPlanet.AddForce(direction * _force, ForceMode2D.Impulse);
+
+         yield return _launchRoutine;
+
+        CreatePlanet();
+        _launchRoutine = null;
     }
 }
