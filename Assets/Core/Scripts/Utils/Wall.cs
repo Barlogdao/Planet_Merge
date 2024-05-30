6cfdaed40957224727e6e2cba945899b52350497
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2024-05-30 18:41:09 +0300

    Add Shaders

---
+++ Assets/Core/Scripts/Utils/Wall.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Wall : MonoBehaviour
+{
+    private const string FadeProperty = "_AlphaTintFade";
+
+    [SerializeField] private float _fadeDuration = 0.2f;
+
+    private Material _material;
+    private int _fadePropertyID;
+    private float _maxFadeValue = 1f;
+    private float _minFadeValue = 0f;
+
+    void Start()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+
+        _fadePropertyID = Shader.PropertyToID(FadeProperty);
+
+        _material.SetFloat(_fadePropertyID, _minFadeValue);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _material.SetFloat(_fadePropertyID, _maxFadeValue);
+        _material.DOFloat(_minFadeValue, _fadePropertyID, _fadeDuration);
+    }
+}
